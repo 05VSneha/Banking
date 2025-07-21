546a3ac06a34eb72d74a08d15e9f70cf9dc81287
05VSneha <v.sneha1@s.amity.edu>
2025-07-21 09:46:25 +0530

    create and deposit money

---
+++ banking.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+string accountHolder;
+int accountNumber;
+double balance = 0.0;
+void createAccount() {
+    cout << "Enter Account Holder Name: ";
+    cin.ignore(); // To ignore newline from previous input
+    getline(cin, accountHolder);
+    cout << "Enter Account Number: ";
+    cin >> accountNumber;
+    cout << "Enter Initial Balance: ";
+    cin >> balance;
+    cout << "\nAccount created successfully!\n";
+void deposit() {
+    double amount;
+    cout << "Enter amount to deposit: ";
+    cin >> amount;
+
+    if (amount > 0) {
+        balance += amount;
+        cout << "Rs." << amount << " deposited successfully!\n";
+    } else {
+        cout << "Invalid amount! Deposit failed.\n";
+    }
+}
